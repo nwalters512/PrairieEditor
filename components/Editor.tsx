1411bda00b9d9d7c72ce4da7ffec9879299c7c3e
Nathan Walters <nwalters512@gmail.com>
2019-05-04 15:50:04 -0500

    Skeleton out file tree and editor layout

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -3,11 +3,23 @@ import * as React from "react";
 const Editor: React.FunctionComponent = () => {
   return (
     <div className="editor">
+      <div className="filetree">File tree</div>
+      <div className="monaco">Monaco editor</div>
       <style jsx>{`
         .editor {
           display: flex;
           flex-direction: row;
           flex: 1 1 0%;
+        }
+        .filetree {
+          width: 300px;
+          background-color: green;
+        }
+        .monaco {
+          display: flex;
+          flex-direction: column;
+          flex: 1 1 0%;
+          background-color: yellow;
         }
       `}</style>
     </div>
