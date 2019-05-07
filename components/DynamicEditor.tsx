7dda283c933d728045234a03de628916585e7c27
Nathan Walters <nwalters512@gmail.com>
2019-05-07 13:56:04 -0500

    Improve editor loading state

--- components/DynamicEditor.tsx
+++ components/DynamicEditor.tsx
@@ -1,6 +1,29 @@
 import dynamic from "next/dynamic";
-const DynamicEditor = dynamic(() => import("./Editor"), { ssr: false });
+import { StyleSheet, css } from "aphrodite";
+import { Spinner } from "reactstrap";
+
+const DynamicEditor = dynamic(() => import("./Editor"), {
+  ssr: false,
+  loading: () => (
+    <div className={css(styles.loading)}>
+      <Spinner color="light" />
+    </div>
+  )
+});
 
 export default () => {
   return <DynamicEditor />;
 };
+
+const styles = StyleSheet.create({
+  loading: {
+    display: "flex",
+    flexDirection: "column",
+    flex: "1 1 0%",
+    justifyContent: "center",
+    alignItems: "center",
+    minWidth: 0,
+    minHeight: 0,
+    backgroundColor: "#1e1e1e"
+  }
+});
